38fdd10fc39c66dc6ffcf84841d4f1e58bac4f10
Carl Elbaz <carl.elbaz@gmail.com>
2014-11-17 14:47:33 -0500

    Get basic game to work

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,33 +1,22 @@
 class Tile
-   def initialize(board)
-      @board
+   def initialize
       @value = "*"
    end
-
 
    def set(value)
      @value = value
    end
-
-   def reveal(coords)
-     if @board.bomb_positions
-
-   end
-
-   def neighbors(coords)
-
-   end
-
-   def neighbor_bomb_count(coords)
 
    attr_reader :value
 end
 
 class Board
     def initialize(size, bombs)
-        @board = Array.new(size)  {Array.new(size,Tile.new(self))}
+        @board = Array.new(size)  {Array.new(size){Tile.new}}
         @size = size
         @bomb_positions = spawn_bombs(bombs)
+        @remaining_tiles = size*size
+        @checked_tiles = []
     end
 
     def display
@@ -35,7 +24,7 @@ class Board
         @board.each do |line|
           string = "|"
           line.each do |tile|
-            string += tile.value + "|"
+            string += "#{tile.value}" + "|"
           end
           puts string
           puts "-"*(@size*2 + 1)
@@ -51,8 +40,69 @@ class Board
       bombs
     end
 
+    def reveal_tile(coords)
+      return nil if bomb_here?(coords)
+      queue = [coords]
+      until queue.empty?
+        curr_coord = queue.pop
+        next if @checked_tiles.include?(curr_coord)
+        @checked_tiles << curr_coord
+        @remaining_tiles -= 1
+        current_neighbors = neighbors(curr_coord)
+        current_bomb_count = bomb_count(current_neighbors)
+        if current_bomb_count == 0
+          queue += current_neighbors
+          self[curr_coord].set("X")
+        else
+          # puts "#{current_bomb_count} BC"
+          self[curr_coord].set(current_bomb_count)
+        end
+      end
+      true
+    end
+
+    MOVES =[
+      [0,1],
+      [0,-1],
+      [1,0],
+      [-1,0],
+      [1,1],
+      [-1,-1],
+      [-1,1],
+      [1,-1]
+    ]
+    def neighbors(current_tile)
+        result = []
+        MOVES.each do |coords|
+          x = coords.first + current_tile.first
+          y = coords.last + current_tile.last
+          if x.between?(0,@size-1) && y.between?(0,@size-1)
+            result << [x,y]
+          end
+        end
+        result
+    end
+
+    def bomb_count(neighbor_arr)
+      result = 0
+      neighbor_arr.each do |neighbor|
+        result += 1 if bomb_here?(neighbor)
+      end
+      result
+    end
+
+    def bomb_here?(coords)
+      @bomb_positions.include?(coords)
+    end
     attr_reader :bomb_positions
 
+    def [](co)
+      @board[co.first][co.last]
+    end
+
+    def win?
+      @remaining_tiles == @bomb_positions.count
+    end
 
 end
 
@@ -62,16 +112,24 @@ class Game
       @size = size
     end
 
+    attr_reader :gameboard
+
     def run
-        until bomb_exploded? || win?
+        until @gameboard.win?
           @gameboard.display
           puts "Please select a tile in the format [0..#{@size-1},0..#{@size-1}]"
-          input = gets.chomp.split(",")
-
+          input = get_move
+          puts "Reveal or flag?(r/f)"
+          type = gets.chomp
+          break if @gameboard.reveal_tile(input).nil?
         end
+        return "YOU WIN!!!" if @gameboard.win?
+        return "YOU LOSE!!"
     end
+
     def get_move
         input = gets.chomp.split(",")
-        [input[0],input[1]]
+        [input[0].to_i,input[1].to_i]
     end
+
 end
