0ca6375cee05af5e0b4972c8260ff8403e452883
carl&julian <c&j@appacademy.io>
2014-11-18 11:04:35 -0500

    define moves for bishop, rook and queen

--- pieces.rb
+++ pieces.rb
@@ -1,20 +1,45 @@
 class Piece
-  def initialize
+  def initialize(board,coord)
+    @board = board
+    @coord = coord
   end
 
   def move
-
   end
 end
 
-class SlidingPiece
+class SlidingPiece < Piece
+    def initialize(type, board)
+        super(board)
+        @type = type
+        init_move(type)
+    end
+
+    def move
+      positions = []
+      if type == "R"
+        positions = r_move
+      elsif type =="B"
+        postions = b_move
+      else
+        postions = r_move + b_move
+      end
+      positions
+    end
+
+    def b_move
+
+    end
+
+    def r_move
+
+    end
+end
+
+class SteppingPiece < Piece
 
 end
 
-class SteppingPiece
-
-end
-
-class Pawn
+class Pawn < Piece
 
 end
